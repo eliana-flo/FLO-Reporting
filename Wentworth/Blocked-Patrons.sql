03135f810bb73c2afc0c4d22fa130af48053961d
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-04-17 11:58:54 -0400

    Update Blocked-Patrons.sql

--- Wentworth/Blocked-Patrons.sql
+++ Wentworth/Blocked-Patrons.sql
@@ -10,13 +10,13 @@ RETURNS TABLE
     barcode text,
     user_email text,
     group_name text,
-    count integer, 
-    sum integer,
+    loan_count integer, 
+    fine_fee_balance integer,
     block_reason text
   )
---This report finds patrons in the Wentworth system that meet the block criteria, not necessarily patrons that currently have a block
+
 AS $$
-select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id), sum(faa.account_balance), 'Max checkouts exceeded' as block_reason from 
+select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id) as loan_count, sum(faa.account_balance) as fine_fee_balance, 'Max checkouts exceeded' as block_reason from 
 folio_derived.users_groups ug
 left join folio_derived.loans_items li on (li.user_id = ug.user_id) 
 left join folio_derived.feesfines_accounts_actions faa on (faa.user_id = ug.user_id) 
@@ -24,7 +24,7 @@ where loan_status = 'Open' and (ug.group_name = 'Alumnus' or ug.group_name = 'FL
 group by ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, faa.account_balance
 having count(loan_id) = 15
 UNION
-select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id), sum(faa.account_balance), 'Max checkouts exceeded' as block_reason from 
+select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id) as loan_count, sum(faa.account_balance) as fine_fee_balance, 'Max checkouts exceeded' as block_reason from 
 folio_derived.users_groups ug
 left join folio_derived.feesfines_accounts_actions faa on (faa.user_id = ug.user_id) 
 left join folio_derived.loans_items li on (li.user_id = ug.user_id) 
@@ -32,7 +32,7 @@ where loan_status = 'Open' and ug.group_name = 'Student'
 group by ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.group_name, faa.account_balance, ug.user_email
 having count(loan_id) = 50
 UNION
-select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id), sum(faa.account_balance), 'Max checkouts exceeded' as block_reason from 
+select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id) as loan_count, sum(faa.account_balance) as fine_fee_balance, 'Max checkouts exceeded' as block_reason from 
 folio_derived.users_groups ug
 left join folio_derived.feesfines_accounts_actions faa on (faa.user_id = ug.user_id) 
 left join folio_derived.loans_items li on (li.user_id = ug.user_id) 
@@ -40,7 +40,7 @@ where loan_status = 'Open' and ug.group_name in ('Library Staff', 'Graduate', 'S
 group by ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.group_name, faa.account_balance, ug.user_email
 having count(loan_id) = 100
 UNION
-select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id), sum(faa.account_balance), 'Max lost items exceeded' as block_reason from 
+select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id) as loan_count, sum(faa.account_balance) as fine_fee_balance, 'Max lost items exceeded' as block_reason from 
 folio_derived.users_groups ug
 left join folio_derived.feesfines_accounts_actions faa on (faa.user_id = ug.user_id) 
 left join folio_derived.loans_items li on (li.user_id = ug.user_id) 
@@ -48,7 +48,7 @@ where loan_status = 'Open' and li.item_status like '%lost%' and ug.group_name in
 group by ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.group_name, faa.account_balance, ug.user_email
 having count(loan_id) = 10
 UNION
-select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id), sum(faa.account_balance), 'Max lost items exceeded' as block_reason from 
+select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id) as loan_count, sum(faa.account_balance) as fine_fee_balance, 'Max lost items exceeded' as block_reason from 
 folio_derived.users_groups ug
 left join folio_derived.feesfines_accounts_actions faa on (faa.user_id = ug.user_id) 
 left join folio_derived.loans_items li on (li.user_id = ug.user_id) 
@@ -56,7 +56,7 @@ where loan_status = 'Open' and li.item_status like '%lost%' and ug.group_name = 
 group by ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.group_name, faa.account_balance, ug.user_email
 having count(loan_id) = 1
 UNION
-select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id), sum(faa.account_balance), 'Max fine balance exceeded' as block_reason
+select ug.user_id, ug.user_last_name, ug.user_first_name, ug.barcode, ug.user_email, ug.group_name, count(loan_id) as loan_count, sum(faa.account_balance) as fine_fee_balance, 'Max fine balance exceeded' as block_reason
 from folio_derived.users_groups ug
 left join folio_derived.loans_items li on (li.user_id = ug.user_id) 
 left join folio_derived.feesfines_accounts_actions faa on (faa.user_id = ug.user_id) 
