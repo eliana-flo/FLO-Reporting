1795ca2e5e219a3416c9358ee3abee98c7b29352
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-04-17 13:08:56 -0400

    Create Demo.sql

---
+++ Wentworth/Demo.sql
@@ -0,0 +1,21 @@
+--metadb:function get_users
+
+DROP FUNCTION IF EXISTS get_users;
+
+CREATE FUNCTION get_users(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01')
+RETURNS TABLE(
+    user_id uuid,
+    barcode text,
+    created_date timestamptz)
+AS $$
+SELECT user_id,
+       barcode,
+       created_date
+    FROM folio_derived.users_groups
+    WHERE start_date <= created_date AND created_date < end_date
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
