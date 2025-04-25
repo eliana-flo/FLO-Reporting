c9aa6489a26427d8f766505c1f04ac9eef4db07b
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-04-25 13:42:38 -0400

    Create booksByLocation.sql

---
+++ Wentworth/booksByLocation.sql
@@ -0,0 +1,21 @@
+--metadb:function booksByLocation
+
+DROP FUNCTION IF EXISTS booksByLocation;
+
+CREATE FUNCTION booksByLocation()
+RETURNS TABLE(location as text,
+item_count as integer)
+AS $$
+SELECT 
+l.name as location, 
+COUNT (it.id) as item_count
+FROM 
+folio_inventory.item__t it
+LEFT JOIN folio_inventory.location__t l ON (l.id = it.effective_location_id)
+LEFT JOIN folio_inventory.material_type__t m ON (m.id = it.material_type_id)
+WHERE m.name = 'Book'
+GROUP BY l.name
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
