8ef7536d1ce84f8b5f94ef03f12e77d8fbfe8f43
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-11-20 16:45:34 -0500

    Update circStatsByDate.sql

--- Wentworth/circStatsByDate.sql
+++ Wentworth/circStatsByDate.sql
@@ -18,8 +18,7 @@ LEFT JOIN folio_inventory.holdings_record__t hrt ON (hrt.instance_id = it.id)
 LEFT JOIN folio_inventory.item__t it2 ON (it2.holdings_record_id = hrt.id)
 LEFT JOIN folio_circulation.loan__t__ lt2 ON (lt2.item_id = it2.id)
 LEFT JOIN folio_inventory.location__t lt ON (lt.id = lt2.item_effective_location_id_at_check_out)
-WHERE '2023-01-01' <= lt2.loan_date  
-AND lt.code = 'stk'
+WHERE start_date <= lt2.loan_date  AND lt2.loan_date < end_date
 AND lt2.action = 'checkedout'
 GROUP BY lt2.item_id, it.title, it2.effective_shelving_order, hrt.call_number, enumeration_data, lt.name, lt2.item_status, lt2.action
   $$
