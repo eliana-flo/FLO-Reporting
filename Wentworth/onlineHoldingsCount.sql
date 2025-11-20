4b411f7680e5f8351186804df15151ddcd2e3608
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2025-11-20 16:30:55 -0500

    Update onlineHoldingsCount.sql

--- Wentworth/onlineHoldingsCount.sql
+++ Wentworth/onlineHoldingsCount.sql
@@ -1,4 +1,16 @@
-SELECT COUNT(hr.id) 
+--metadb:function onlineHoldingsCount
+
+DROP FUNCTION IF EXISTS onlineHoldingsCount;
+
+CREATE FUNCTION onlineHoldingsCount()
+RETURNS TABLE
+  (holdings_count integer)
+AS $$
+SELECT COUNT(hr.id) as holdings_count
 FROM folio_inventory.holdings_record__t hr
 LEFT JOIN folio_inventory.location__t l ON (l.id = hr.permanent_location_id)
 WHERE l.name LIKE 'Online'
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
